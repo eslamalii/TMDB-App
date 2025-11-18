fcd72f1540edf759261176a68057ef78ee4545f0
eslamalii <preslamali@hotmail.com>
2025-11-18 12:54:43 +0200

    feat(cleanup): remove unused AppController and AppService files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { SeedModule } from './seed/seed.module';
@@ -36,9 +34,8 @@ import { APP_GUARD } from '@nestjs/core';
     WatchlistModule,
     AppCacheModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
